4cc9b878a2b27432a5bb83a96b0a2d2606a38798
Hiroshi Murayama <opiopan@gmail.com>
2023-08-17 21:26:28 +0900

    updated

--- docs/_bin/retrieve_release.sh
+++ docs/_bin/retrieve_release.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-ls -ld `dirname ${OUTPUT_FILE}`
-python3 --version
 curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/opiopan/DigViewer/releases > "${OUTPUT_FILE}"
+    https://api.github.com/repos/opiopan/DigViewer/releases |\
+python3 -c "
+    import json, sys
+    data = json.load(sys.stdin, encoding='utf-8')
+    print("<latest>%s</latest>"%(data[0]["name"]))
+" > "${OUTPUT_FILE}"
