c70a277cb7814f4cf3ae3003fe7c68c56311dc12
Hiroshi Murayama <opiopan@gmail.com>
2023-08-19 09:16:26 +0900

    updated workflow 3

--- docs/_bin/retrieve_release.sh
+++ docs/_bin/retrieve_release.sh
@@ -3,12 +3,11 @@ curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/opiopan/${GITHUB_REPOSITORY}/releases |\
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/releases |\
 python3 -c "
 import json, sys, re
 release = [rel for rel in filter(lambda rel: rel['published_at'] and not rel['prerelease'], json.load(sys.stdin))][0]
-print(release)
 version = re.sub('^[vV]', '', release['name'])
 url = [asset for asset in filter(lambda asset: re.search('\.[dD][mM][gG]\$', asset['name']), release['assets'])][0]['browser_download_url']
 print('{\"version\":\"%s\", \"url\":\"%s\"}'%(version, url))
-" #> "${OUTPUT_FILE}"
+" > "${OUTPUT_FILE}"
